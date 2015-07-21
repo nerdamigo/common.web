88d129dc9f54df708c296904e1d7fb56780850f3
matt <matt@nerdamigo.com>
2015-07-20 21:58:49 -0400

    initial commit

---
+++ WebPathMapper.cs
@@ -0,0 +1,17 @@
+﻿using NerdAmigo.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdAmigo.Common.Web
+{
+	public class WebPathMapper : IPathMapper
+	{
+		public string MapPath(string appRelativePath)
+		{
+			return System.Web.HttpContext.Current.Request.MapPath(appRelativePath);
+		}
+	}
+}
